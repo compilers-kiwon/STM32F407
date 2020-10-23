fde29d2e710f890403d801f7d9318f51932ffbf5
Kiwon Kwon <compilers.naver.com>
2020-10-23 11:51:50 +0900

    Blink 4 LDs

--- FreeRTOS/Core/Src/freertos.c
+++ FreeRTOS/Core/Src/freertos.c
@@ -38,6 +38,11 @@
 /* USER CODE BEGIN PD */
 #define	MAX_STATE		2
 #define	MAX_STR_SIZE	10
+#define	NUM_OF_LD		4
+#define	GPIO_LD			GPIOD
+
+#define	get_prev_ld(current)	(((current)+(NUM_OF_LD)-1)%(NUM_OF_LD))
+#define	get_next_ld(current)	(((current)+1)%(NUM_OF_LD))
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -47,7 +52,9 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-const char	state[MAX_STATE][MAX_STR_SIZE] = {"OFF","ON"};
+const uint8_t	state[MAX_STATE][MAX_STR_SIZE] = {"OFF","ON"};
+const uint16_t	LD_GPIO_Pin[NUM_OF_LD] = {GPIO_PIN_12,GPIO_PIN_13,GPIO_PIN_14,GPIO_PIN_15};
+const uint32_t	LD_ON_time[NUM_OF_LD] = {300,500,700,1100};
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
@@ -71,10 +78,18 @@ const osThreadAttr_t LD4Task_attributes = {
   .stack_size = 128 * 4
 };
 
+osThreadId_t LDBlinkTaskHandle;
+const osThreadAttr_t LDBlinkTask_attributes = {
+  .name = "LDBlinkTask",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-void LD3Task(void *argument);
-void LD4Task(void *argument);
+void LD3Task(void* argument);
+void LD4Task(void* argument);
+void LDBlinkTask(void* argument);
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
@@ -113,10 +128,12 @@ void MX_FREERTOS_Init(void) {
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
-  LD3TaskHandle = osThreadNew(LD3Task, NULL, &LD3Task_attributes);
-  printf("LD3Task has been created\n");
-  LD4TaskHandle = osThreadNew(LD4Task, NULL, &LD4Task_attributes);
-  printf("LD4Task has been created\n");
+  //LD3TaskHandle = osThreadNew(LD3Task, NULL, &LD3Task_attributes);
+  //printf("LD3Task has been created\n");
+  //LD4TaskHandle = osThreadNew(LD4Task, NULL, &LD4Task_attributes);
+  //printf("LD4Task has been created\n");
+  LDBlinkTaskHandle = osThreadNew(LDBlinkTask, NULL, &LDBlinkTask_attributes);
+  printf("LDBlinkTask has been created\n");
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
 
@@ -144,32 +161,41 @@ void StartDefaultTask(void *argument)
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
-void LD3Task(void *argument)
+void LD3Task(void* argument)
 {
-  /* init code for USB_HOST */
-  MX_USB_HOST_Init();
-  /* USER CODE BEGIN StartDefaultTask */
-  /* Infinite loop */
   for(;;)
   {
 	HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_13);
 	printf("LD3 %s\n",&state[HAL_GPIO_ReadPin(GPIOD,GPIO_PIN_13)][0]);
     osDelay(500);
   }
-  /* USER CODE END StartDefaultTask */
 }
 
-void LD4Task(void *argument)
+void LD4Task(void* argument)
 {
-  /* init code for USB_HOST */
-  MX_USB_HOST_Init();
-  /* USER CODE BEGIN StartDefaultTask */
-  /* Infinite loop */
   for(;;)
   {
 	HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_12);
 	printf("LD4 %s\n",&state[HAL_GPIO_ReadPin(GPIOD,GPIO_PIN_12)][0]);
     osDelay(700);
+  }
+}
+
+void LDBlinkTask(void* argument)
+{
+  int	current = 0;
+
+  HAL_GPIO_TogglePin(GPIO_LD,LD_GPIO_Pin[current]);
+  printf("%d\n",current);
+  osDelay(LD_ON_time[current]);
+
+  for(;;)
+  {
+	HAL_GPIO_TogglePin(GPIO_LD,LD_GPIO_Pin[current]);
+	current=get_next_ld(current);
+	HAL_GPIO_TogglePin(GPIO_LD,LD_GPIO_Pin[current]);
+	printf("%d\n",current);
+	osDelay(LD_ON_time[current]);
   }
   /* USER CODE END StartDefaultTask */
 }
